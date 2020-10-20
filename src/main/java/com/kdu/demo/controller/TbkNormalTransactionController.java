44365b924db69f534de8758949a16bfe1b18ca90
pedro-quila <kila1213@gmail.com>
2020-10-20 20:48:31 -0300

    Controller updated, CORS annotation

--- src/main/java/com/kdu/demo/controller/TbkNormalTransactionController.java
+++ src/main/java/com/kdu/demo/controller/TbkNormalTransactionController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/normal-transaction")
 public class TbkNormalTransactionController {
 
