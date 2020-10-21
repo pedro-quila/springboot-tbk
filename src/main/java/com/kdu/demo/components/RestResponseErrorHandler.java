e365cb348ed65e94177528a00c5164a138b2fc6c
pedro-quila <kila1213@gmail.com>
2020-10-20 21:32:58 -0300

    corrección manejo de errores

--- src/main/java/com/kdu/demo/components/RestResponseErrorHandler.java
+++ src/main/java/com/kdu/demo/components/RestResponseErrorHandler.java
@@ -1,15 +1,21 @@
 package com.kdu.demo.components;
 
+import com.kdu.demo.controller.TbkNormalTransactionController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.server.ResponseStatusException;
 
+
 import java.io.IOException;
 
 @Component
 public class RestResponseErrorHandler implements ResponseErrorHandler {
+
+    Logger logger = LoggerFactory.getLogger(RestResponseErrorHandler.class);
 
     @Override
     public boolean hasError(ClientHttpResponse httpResponse)
@@ -26,6 +32,7 @@ public class RestResponseErrorHandler implements ResponseErrorHandler {
 
         if (httpResponse.getStatusCode()
                 .series() == HttpStatus.Series.SERVER_ERROR) {
+            logger.error("Falla al iniciar transaccion, server error");
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR, "Falla al iniciar init transaction, server error");
             // handle SERVER_ERROR
@@ -34,12 +41,14 @@ public class RestResponseErrorHandler implements ResponseErrorHandler {
             
             // handle CLIENT_ERROR
             if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+                logger.error("Falla al iniciar transaccion, not found");
                 throw new ResponseStatusException(
                         HttpStatus.NOT_FOUND, "Falla al iniciar init transaction, not found");
             }
 
             // handle CLIENT_ERROR
             if (httpResponse.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                logger.error("Error en autenticación, unauthorized");
                 throw new ResponseStatusException(
                         HttpStatus.UNAUTHORIZED, "Error en autenticación");
             }
--- src/main/java/com/kdu/demo/service/impl/TbkNormalTransactionServiceImpl.java
+++ src/main/java/com/kdu/demo/service/impl/TbkNormalTransactionServiceImpl.java
@@ -8,19 +8,15 @@ import com.kdu.demo.dto.ConfirmTransactionResponse;
 import com.kdu.demo.dto.InitTransactionRequest;
 import com.kdu.demo.dto.InitTransactionResponse;
 import com.kdu.demo.service.TbkNormalTransactionService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class TbkNormalTransactionServiceImpl implements TbkNormalTransactionService {
 
-    Logger logger = LoggerFactory.getLogger(TbkNormalTransactionServiceImpl.class);
+
 
     @Value("${initPost}")
     private String urlInitPost;
@@ -39,29 +35,20 @@ public class TbkNormalTransactionServiceImpl implements TbkNormalTransactionServ
 
     @Override
     public InitTransactionResponse initTransaction(InitTransactionRequest initTransactionRequest) {
-        try {
+
             return transbankRestClientInit.execute(
                     new RequestDetails
                             (urlInitPost,HttpMethod.POST), initTransactionRequest, restResponseErrorHandler,
                     InitTransactionResponse.class);
-        } catch (Exception e) {
-            logger.error("Excepcion ", e);
-            throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR, "Falla al iniciar transaccion", e);
-        }
+
     }
 
     @Override
     public ConfirmTransactionResponse confirmTransaction(ConfirmTransactionRequest confirmTransactionRequest) {
-        try {
             return transbankRestClientConfirm.execute(
                     new RequestDetails
                             (urlConfirmPut.concat(confirmTransactionRequest.getToken()),
                                     HttpMethod.POST), confirmTransactionRequest, restResponseErrorHandler, ConfirmTransactionResponse.class);
-        } catch (Exception e) {
-            logger.error("Excepcion ", e);
-            throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR, "Falla al confirmar transaccion", e);
-        }
+
     }
 }
